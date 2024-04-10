fc3ea7822a847d2d9472eb04674678daacbc4436
goyalsaanvi1 <105721424+goyalsaanvi1@users.noreply.github.com>
2024-04-09 22:02:30 -0400

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -1,16 +1,4 @@
-/*
-    Course: CSC 220-02
-    Term: Fall 2022
-    Assignment: Project 1
-    Instructor: John DeGood
-    Student: Saanvi Goyal 
-    
-    The user plays Hangman with six letter words only. The game starts with 3 lives. The user gets a prompt asking them to guess a letter. 
-    Lives and scores are increased when the letter is guessed correctly, and decreased when guessed incorrectly and the same letter is guessed again. 
-    The currentWord updates after every correct guess. After each guess the updated number of lives and scores is printed out. 
-    At the end of the game, a game over message, updated score, and the word is printed. It also asks the player if they want to play again or not. 
-*/
-import java.util.Arrays;
+ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 public class Hangman {
